f52884e9d15e7ed0c5de8e9037f3483576b2a0a2
Pascal Khuu <pascal.khuu@orange.fr>
2022-05-01 19:23:40 +0200

    validators front page inscription

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -7,20 +7,32 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-  signUpForm!:FormGroup;
+  inscription!:FormGroup;
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.signUpForm = this.fb.group({
+    this.inscription = this.fb.group({
       name: ['', Validators.required],
       firstName: ['', Validators.required],
-      userName: ['', [Validators.required,Validators.minLength(12), Validators.maxLength(30)]],
+      userName: ['', [Validators.required,Validators.minLength(12), Validators.maxLength(30),Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(12)]],
+      
   })
+      // {
+      //   validator: this.passwordMatchValidator
+      // })
  }
   onSubmitForm() {
-    console.log(this.signUpForm.value);
+    console.log(this.inscription.value);
   }
+  // passwordMatchValidator(form: FormGroup) {
+     
+  //   if (form.get('password')?.value === form.get('confirmPassword')?.value) {
+  //     return null;
+  //   } else {
+  //     return { mismatch: true };
+  //   }
+  // }
 }
 
 
